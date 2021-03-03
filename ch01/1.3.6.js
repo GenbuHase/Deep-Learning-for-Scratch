f960c0b633600c074d82ea033e0890d3f2178592
Genbu Hase <14960841+GenbuHase@users.noreply.github.com>
2021-03-03 12:08:30 +0900

    1.0.6 + ch01/1.3.6 終了

---
+++ ch01/1.3.6.js
@@ -0,0 +1,7 @@
+const hungry = true;
+const sleepy = false;
+
+console.log(typeof hungry); // boolean
+console.log(!hungry); // false
+console.log(hungry && sleepy); // false
+console.log(hungry || sleepy); // true
